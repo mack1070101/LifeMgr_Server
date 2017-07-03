f360cbab86c9a9349c4c65caf409b283f38d8d72
mackenzie <mackenziebligh@gmail.com>
2017-07-03 15:46:23 -0600

    WAR is now deployable and working

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,18 +2,19 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-/**
- * Created by mackenzie on 02/07/17.
- */
-
-
-@ComponentScan({"controllers","models"})
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
 }
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,25 @@
+package hello;
+
+/**
+ * Created by mackenzie on 02/07/17.
+ *
+ * Sample class to test functionaltiy of server
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
