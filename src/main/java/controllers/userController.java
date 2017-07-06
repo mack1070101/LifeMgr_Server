8c778f1afc78eb6f55bad1e01358a96b3455b595
mackenzie <mackenziebligh@gmail.com>
2017-07-06 16:33:34 -0600

    Add Pram mappings, and user mappings

--- src/main/java/controllers/userController.java
+++ src/main/java/controllers/userController.java
@@ -1,7 +1,29 @@
 package controllers;
 
+import models.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 /**
  * Created by mackenzie on 04/07/17.
  */
+@RequestMapping("/user")
 public class userController {
+
+    @RequestMapping(name="/create", method=POST)
+    public User user(@RequestParam String username,
+                     @RequestParam String firstName,
+                     @RequestParam String lastName,
+                     @RequestParam String dateOfBirth,
+                     @RequestParam String dateOfBirthFormat,
+                     @RequestParam(defaultValue = "", required = false) String timePreference,
+                     @RequestParam(defaultValue = "", required = false) String datePreference,
+                     @RequestParam(defaultValue = "", required = false) String location) {
+
+        return new User(username, firstName,lastName, dateOfBirth, dateOfBirthFormat,
+                        timePreference, datePreference, location);
+    }
 }
